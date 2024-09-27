5f474603f5dc8a49d6d2dda0e0a61fc127708421
Nicolas Giglio <nicosgiglio@gmail.com>
2024-09-26 21:08:37 -0300

    se modulariza la funcion es_par

--- par.py
+++ par.py
@@ -1,7 +1,11 @@
+def es_par(num):
+    return num%2==0
+
 def main():
+
     num= int(input("ingrese numero para evaluar paridad: "))
-    es_par= num%2==0
-    if es_par:
+    
+    if es_par(num):
         print("es par")
     else:
         print("es impar")
