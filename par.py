90ffc893917f040be6eecfc4efcf1426c4770e34
Nicolas Giglio <nicosgiglio@gmail.com>
2024-09-26 21:36:44 -0300

    validar ingreso positivo

--- par.py
+++ par.py
@@ -1,10 +1,16 @@
 def es_par(num):
     return num%2==0
 
+def solicitar_numero():
+    num= int(input("ingrese numero para evaluar paridad: "))
+    while num<=0:
+        print("ingreso invalido")
+        num= int(input("ingrese numero para evaluar paridad: "))
+
 def main():
 
-    num= int(input("ingrese numero para evaluar paridad: "))
-    
+    num= solicitar_numero()
+
     if es_par(num):
         print("es par")
     else:
